4258788c7843bc74a2b3babc746ab8090764e482
Quentin <91956889+SadeQL@users.noreply.github.com>
2022-01-24 17:06:07 +0100

    several tentatives but failed

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const userInput = (userpassword) => {
-  userpassword = document.getElementById("userpassword");
-  /* alert("password taken into account"); */
-  return userpassword;
-};
+/* import { randomBytes } from "crypto"; */
+import { subtle, randomBytes } from "crypto";
 
-const hashGenerator = (userpassword) => {
+const hashGenerator = async () => {
+  const userpassword = document.getElementById("userpassword").value;
+  console.log(userpassword);
   const utf8 = new TextEncoder().encode(userpassword);
-  return crypto.subtle.digest("SHA-256", utf8).then((hashBuffer) => {
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-      .map((bytes) => bytes.toString(16).padStart(2, "0"))
-      .join("");
-    document.getElementById("result").innerHTML = hashHex;
-    console.log(hashHex);
-    return hashHex;
-  });
+  const hashBuffer = await subtle.digest("SHA-256", utf8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((bytes) => bytes.toString(16).padStart(2, "0"))
+    .join("");
+  console.log(hashHex);
+  return hashHex;
 };
 
-hashGenerator(userInput());
+hashGenerator();
+
+function hashPassword(password) {
+  var salt = randomBytes(128).toString("base64");
+  var iterations = 10000;
+  var hash = hashGenerator(password, salt, iterations);
+  console.log(password);
+
+  return {
+    salt: salt,
+    hash: hash,
+    iterations: iterations,
+  };
+}
+
+hashPassword();
