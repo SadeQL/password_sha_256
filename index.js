142a7325240fbe56933efef78bd01e4ccc677639
YanissB <yaniss.besson@student.adatechschool.fr>
2022-01-25 12:01:53 +0100

    hash and salt done

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-/* import { randomBytes } from "crypto"; */
-import { subtle, randomBytes } from "crypto";
-
-const hashGenerator = async () => {
+function hashPassword() {
   const userpassword = document.getElementById("userpassword").value;
-  console.log(userpassword);
-  const utf8 = new TextEncoder().encode(userpassword);
-  const hashBuffer = await subtle.digest("SHA-256", utf8);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((bytes) => bytes.toString(16).padStart(2, "0"))
-    .join("");
-  console.log(hashHex);
-  return hashHex;
-};
-
-hashGenerator();
-
-function hashPassword(password) {
-  var salt = randomBytes(128).toString("base64");
+  var salt = CryptoJS.lib.WordArray.random(128 / 8);
   var iterations = 10000;
-  var hash = hashGenerator(password, salt, iterations);
-  console.log(password);
+  var hash = CryptoJS.PBKDF2(userpassword, salt, iterations);
+  var key128Bits = CryptoJS.PBKDF2(hash, salt, {
+     keySize: 128 / 32
+    });
 
   return {
     salt: salt,
-    hash: hash,
+    hash: hash.toString(CryptoJS.enc.Base64),
     iterations: iterations,
+    key128Bits: key128Bits
   };
 }
-
-hashPassword();
